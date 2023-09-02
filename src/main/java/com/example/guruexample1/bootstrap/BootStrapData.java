d788c2d69a2ae82dd2d30bda77569533eb721069
Oktaykarabiyik <77680642+Oktaykarabiyik@users.noreply.github.com>
2023-09-03 00:23:33 +0300

    Publisher added

--- src/main/java/com/example/guruexample1/bootstrap/BootStrapData.java
+++ src/main/java/com/example/guruexample1/bootstrap/BootStrapData.java
@@ -1,9 +1,11 @@
 package com.example.guruexample1.bootstrap;
 
+import com.example.guruexample1.Publisher;
 import com.example.guruexample1.domain.Author;
 import com.example.guruexample1.domain.Book;
 import com.example.guruexample1.domain.repositories.AuthorRepository;
 import com.example.guruexample1.domain.repositories.BookRepository;
+import com.example.guruexample1.domain.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,14 +14,24 @@ public class BootStrapData implements CommandLineRunner {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+    public BootStrapData(BookRepository bookRepository, AuthorRepository authorRepository, PublisherRepository publisherRepository) {
         this.bookRepository = bookRepository;
         this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher =new Publisher();
+
+        publisher.setName("Oktay");
+        publisher.setName("Kastamonu");
+
+        publisherRepository.save(publisher);
+        System.out.println(("Number of Publisher: "+publisherRepository.count()));
 
         Author eric = new Author("Eric","Evans");
         Book ddd=new Book("Domain Driven Design","123123");
@@ -32,6 +44,7 @@ public class BootStrapData implements CommandLineRunner {
 
         System.out.println("Started in Bootstrap");
         System.out.println(("Number of Books: "+bookRepository.count()));
+
 
     }
 }
---
+++ src/main/java/com/example/guruexample1/domain/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.example.guruexample1.domain.repositories;
+
+import com.example.guruexample1.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher,Long> {
+
+}
